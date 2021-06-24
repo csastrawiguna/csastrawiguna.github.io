a54d2a8eb3ede5ad035d8022d5b711ae57e8bef6
csastrawiguna <76550847+csastrawiguna@users.noreply.github.com>
2021-06-24 12:23:30 +0700

    Add files via upload

---
+++ Service Area/js/alamat.js
@@ -0,0 +1,54 @@
+
+const tombolCopy = document.querySelectorAll('tr button');
+const alamatCopy = document.querySelectorAll('tr td:nth-child(2)');
+
+for(let i = 0; i < tombolCopy.length; i++){
+	tombolCopy[i].addEventListener('click',copy());
+};
+
+
+function copy(){
+	alamatCopy[0].select();
+	document.execCommand("copy");
+	console.log("hasil copy: " + alamatCopy[0].value);
+}
+
+(function(document) {
+	'use strict';
+
+	var LightTableFilter = (function(Arr) {
+
+		var _input;
+
+		function _onInputEvent(e) {
+			_input = e.target;
+			var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+			Arr.forEach.call(tables, function(table) {
+				Arr.forEach.call(table.tBodies, function(tbody) {
+					Arr.forEach.call(tbody.rows, _filter);
+				});
+			});
+		}
+
+		function _filter(row) {
+			var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
+			row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
+		}
+
+		return {
+			init: function() {
+				var inputs = document.getElementsByClassName('light-table-filter');
+				Arr.forEach.call(inputs, function(input) {
+					input.oninput = _onInputEvent;
+				});
+			}
+		};
+	})(Array.prototype);
+
+	document.addEventListener('readystatechange', function() {
+		if (document.readyState === 'complete') {
+			LightTableFilter.init();
+		}
+	});
+
+})(document);
